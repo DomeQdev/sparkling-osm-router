5b66be5502e1c3ca1b9725be7edd3f542f6eb689
♿ <domeqrecords+gh@gmail.com>
2026-04-17 18:55:52 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import Graph, { type GraphOptions } from "./Graph";
+import Graph, { type BuildGraphOptions } from "./Graph";
 import offsetShape from "./tools/offsetShape";
 import simplifyShape from "./tools/simplifyShape";
 
 export * from "./typings";
-export { Graph, type GraphOptions, offsetShape, simplifyShape };
+export { Graph, type BuildGraphOptions, offsetShape, simplifyShape };
